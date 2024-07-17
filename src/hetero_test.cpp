fec446772d02f897ca1143fafe1f35c51243e08a
HPDell <huyg0180110559@outlook.com>
2024-07-17 12:11:41 +0100

    edit: improve performance when sample number is low

--- src/hetero_test.cpp
+++ src/hetero_test.cpp
@@ -46,24 +46,15 @@ mat denreg_poly(
 mat denreg_poly(
     const mat& x,
     const mat& uv,
-    const mat& sw,
+    const cube& L,
     size_t poly = 2,
     double bw = 10,
     int kernel = 0
 ) {
-    uword ndp = uv.n_rows, dim = uv.n_cols;
     mat g(arma::size(x));
     for (uword i = 0; i < x.n_rows; i++) {
-        mat duv = (uv.each_row() - uv.row(i));
-        mat wi = sw.col(i);
-        mat U(ndp, dim * poly + 1, fill::ones);
-        for (size_t p = 0; p < poly; p++) {
-            U.cols(p * dim + 1, p * dim + dim) = pow(duv, p + 1);
-        }
-        mat Utw = trans(U.each_col() % wi);
-        mat UtwU = Utw * U;
         for (uword k = 0; k < x.n_cols; k++) {
-            vec r = solve(UtwU, Utw * x.col(k));
+            vec r = L.slice(i) * x.col(k);
             g(i, k) = r(0);
         }
     }
@@ -81,23 +72,30 @@ List spatial_hetero_perm(
     int verbose = 0
 ) {
     bool precalc_dw = false;
-    mat sw(0, 0);
+    uword ndp = uv.n_rows, dim = uv.n_cols, ncL = dim * poly + 1;
+    cube L(ncL, ndp, ndp);
     uword n = uv.n_rows;
-    if (n < 10000) {
+    if (n < 4096) {
         precalc_dw = true;
         if (verbose > 0) Rcout << "* Calculating spatial weights in advance" << "\n";
-        sw = mat(n, n, arma::fill::zeros);
         for (uword i = 0; i < n; i++)
         {
             mat duv = uv.each_row() - uv.row(i);
             vec d = sqrt(sum(duv % duv, 1));
-            sw.col(i) = kernel_bisquare_ada(d, bw, i);
+            vec wi = kernel_bisquare_ada(d, bw, i);
+            mat U(ndp, ncL, arma::fill::ones);
+            for (size_t p = 0; p < poly; p++) {
+                U.cols(p * dim + 1, p * dim + dim) = pow(duv, p + 1);
+            }
+            mat Utw = trans(U.each_col() % wi);
+            mat UtwU_inv = inv(Utw * U);
+            L.slice(i) = UtwU_inv * Utw;
         }
         if (verbose > 0) Rcout << "* Testing with pre-calculated spatial weights" << "\n";
     } else {
         if (verbose > 0) Rcout << "* Testing without pre-calculated spatial weights" << "\n";
     }
-    mat r0 = precalc_dw ? denreg_poly(x, uv, sw, poly, bw, kernel) : denreg_poly(x, uv, poly, bw, kernel);
+    mat r0 = precalc_dw ? denreg_poly(x, uv, L, poly, bw, kernel) : denreg_poly(x, uv, poly, bw, kernel);
     // mat r0 = denreg_poly(x, uv, poly, bw, kernel);
     rowvec stat0 = var(r0, 0, 0);
     mat stats(resample, x.n_cols);
@@ -109,7 +107,7 @@ List spatial_hetero_perm(
         {
             xi.col(c) = shuffle(x.col(c));
         }
-        mat ri = precalc_dw ? denreg_poly(xi, uv, sw, poly, bw, kernel) : denreg_poly(xi, uv, poly, bw, kernel);
+        mat ri = precalc_dw ? denreg_poly(xi, uv, L, poly, bw, kernel) : denreg_poly(xi, uv, poly, bw, kernel);
         // mat ri = denreg_poly(xi, uv, poly, bw, kernel);
         stats.row(i) = var(ri, 0, 0);
         p.tic();
