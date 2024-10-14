c546f4a09537555eb278a7853a1a1c0b7a6a143e
Yigong Hu <huyg0180110559@outlook.com>
2024-10-14 17:41:09 +0100

    Feat: spatial heterogeneity test based on bootstrap (#24)
    * edit: spatial heterogeneity test use bootstrap
    
    * add: spatial.hetero generic
    
    * fix: code style
    
    * edit: spatial hetero test api
    
    * fix: remove spatial hetero test from summary.hgwrm
    
    * fix: spatial hetero function names
    
    * add: tests for spatial heterogeneity
    
    * edit: call of spatial_hetero_test on data
    
    * fix: handle ... in spatial_hetero_test.hgwrm
    
    * edit: parallel only accept boolean numbers
    
    * fix: errors in spatial hetero test
    
    * fix: check errors

--- tests/others/only_random_intercept.R
+++ tests/others/only_random_intercept.R
@@ -1,8 +1,8 @@
 data(multisampling)
 m <- hgwr(
-    formula = y ~ L(g1 + g2) + x1 + (1 | group),
-    data = multisampling$data,
-    coords = multisampling$coords,
-    alpha = 1e-8,
-    verbose = 1
+  formula = y ~ L(g1 + g2) + x1 + (1 | group),
+  data = multisampling$data,
+  coords = multisampling$coords,
+  alpha = 1e-8,
+  verbose = 1
 )
--- tests/testthat/test-hetero.R
+++ tests/testthat/test-hetero.R
@@ -1,18 +1,45 @@
 library(testthat)
 
-test_that("Multisampling", {
-    data(multisampling)
-    m <- expect_no_error({hgwr(
-        formula = y ~ L(g1 + g2) + x1 + (z1 | group),
-        data = multisampling$data,
-        coords = multisampling$coords,
-        bw = 10,
-        alpha = 1e-8
-    )})
-    expect_no_error({
-        summary(m, test_hetero = TRUE)
-    })
-    expect_no_error({
-        summary(m, test_hetero = list(kernel = "gaussian"))
-    })
+test_that("spatial heterogeneity: vector matrix data.frame", {
+  data(multisampling)
+  g <- with(multisampling, {
+    aggregate(data[c("g1", "g2")], by = list(data$group), mean)
+  })[,-1]
+  expect_no_error({
+    spatial_hetero_test_data(g, as.matrix(multisampling$coords))
+  })
+  expect_no_error({
+    spatial_hetero_test(as.matrix(g), multisampling$coords)
+  })
+  expect_no_error({
+    spatial_hetero_test(g[["g1"]], multisampling$coords)
+  })
+  expect_no_error({
+    spatial_hetero_test(g, multisampling$coords)
+  })
+})
+
+test_that("spatial heterogeneity: sf", {
+  data(wuhan.hp)
+  g <- aggregate(wuhan.hp, list(wuhan.hp$group), mean)[1:16, -1]
+  g <- g[c("d.Commercial", "d.GreenLand")]
+  expect_no_error({
+    spatial_hetero_test(g)
+  })
+})
+
+test_that("spatial heterogeneity: HGWR", {
+  data(multisampling)
+  m <- expect_no_error({
+    hgwr(
+      formula = y ~ L(g1 + g2) + x1 + (z1 | group),
+      data = multisampling$data,
+      coords = multisampling$coords,
+      bw = 10,
+      alpha = 1e-8
+    )
+  })
+  expect_no_error({
+    spatial_hetero_test(m)
+  })
 })
