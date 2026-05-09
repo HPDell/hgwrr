d6fe5cbceddb1b6be9bf1c84acd2d1e709efc426
HPDell <huyg0180110559@outlook.com>
2026-05-09 12:22:19 +0800

    edit: default mcmc options

--- R/options.R
+++ R/options.R
@@ -4,16 +4,16 @@ ml_options <- function(
   ml_type = c("D_Only", "D_Beta")
 ) list(
   method = "ML",
-  alpha = 0.01,
-  eps_gradient = 1e-6,
+  alpha = alpha,
+  eps_gradient = eps_gradient,
   ml_type = match.arg(ml_type)
 )
 
 mcmc_options <- function(
-  mcmc_iters = 10000,
-  mcmc_burnin = 2000
+  mcmc_iters = 5000,
+  mcmc_burnin = 400
 ) list(
   method = "MCMC",
-  mcmc_iters = 10000,
-  mcmc_burnin = 2000
+  mcmc_iters = mcmc_iters,
+  mcmc_burnin = mcmc_burnin
 )
